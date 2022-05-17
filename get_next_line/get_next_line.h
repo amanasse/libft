c1a57682955d894169dc18d707dc77dfd7ff03f3
Adrian MANASSERO <amanasse@e3r12p13.clusters.42paris.fr>
2022-05-17 12:38:58 +0200

    get next line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amanasse <amanasse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/17 12:32:34 by amanasse          #+#    #+#             */
+/*   Updated: 2022/05/17 12:33:15 by amanasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <string.h>
+# include <strings.h>
+# include <ctype.h>
+# include <unistd.h>
+
+
+
+
+#endif
